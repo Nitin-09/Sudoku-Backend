a28a9b9fcb58a97a91ed17f1adb4c1680cae3cf9
Nitin-09 <nitingangwani982000@gmail.com>
2023-06-13 14:53:46 +0530

    changes

--- functions/db.js
+++ functions/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-const mongoURI ="mongodb+srv://nitingangwani982000:Gangwani098@cluster0.48p0wxa.mongodb.net/sudoku"
+const mongoURI ="mongodb+srv://nitingangwani982000:Nitin09@cluster0.tztc33s.mongodb.net/sudoku"
 const connectToMongo =()=>{
     mongoose.connect(mongoURI,{useNewUrlParser: true, useUnifiedTopology: true},()=>{
         console.log("connected")
